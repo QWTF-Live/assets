8e35089ae8edb40e918d4e3c41234643afdfe2c5
8bitprodigy <chrisxdeboy@gmail.com>
2025-05-06 19:40:05 -0700

    Replaced Assault Cannon model Replaced Assault Cannon sounds Updated qwtfl_2fort added dev_turbine added dev_turbine_pro

--- texture-wads/compilewads.sh
+++ texture-wads/compilewads.sh
@@ -2,8 +2,8 @@ qpakman lq_conc/*.png -o lq_conc.wad
 qpakman lq_dev/*.png -o lq_dev.wad
 qpakman lq_greek/*.png -o lq_greek.wad
 qpakman lq_health_ammo/*.png -o lq_health_ammo.wad
-qpakman lq_liquidsky/*.png -o lq_liquidsky.wad
-qpakman lq_mayan/*.png -o lq_mayan.wad
+qpakman lq_liquidsky/*.png -o qwtfl_liquidsky.wad
+qpakman lq_mayan/*.png -o qwtfl_mayan.wad
 qpakman lq_medieval/*.png -o lq_medieval.wad
 qpakman lq_metal/*.png -o lq_metal.wad
 qpakman lq_palette/*.png -o lq_palette.wad
@@ -12,4 +12,5 @@ qpakman lq_tech/*.png -o lq_tech.wad
 qpakman lq_terra/*.png -o lq_terra.wad
 qpakman lq_utility/*.png -o lq_utility.wad
 qpakman lq_wood/*.png -o lq_wood.wad
+qpakman qwtfl_fence/*.png -o qwtfl_fence.wad
 
